be7feb11bbc376e1fc0013e8e883034d93842e21
codeguru <139157705+codeguru185@users.noreply.github.com>
2023-09-19 11:26:15 -0500

    Create new.py

---
+++ new.py
@@ -0,0 +1,55 @@
+
+1. **Credit Bureaus**:
+   - Equifax
+   - Experian
+   - TransUnion
+
+2. **Transaction Data Providers**:
+   - Fiserv
+   - ACI Worldwide
+   - First Data (now part of Fiserv)
+
+3. **Identity Verification Services**:
+   - Jumio
+   - Trulioo
+   - Onfido
+
+4. **Geolocation Data**:
+   - MaxMind
+   - Neustar
+   - IPinfo
+
+5. **Device Fingerprinting**:
+   - ThreatMetrix (a LexisNexis Risk Solutions company)
+   - iovation (a TransUnion company)
+   - BioCatch
+
+6. **Behavioral Biometrics**:
+   - BioCatch
+   - BehavioSec
+   - NuData Security (a Mastercard company)
+
+7. **IP Reputation Services**:
+   - Spamhaus
+   - Barracuda Central
+   - Cisco Talos
+
+8. **Payment Processor Data**:
+   - PayPal
+   - Stripe
+   - Square
+
+9. **Social Media Data**:
+   - Various social media platforms (e.g., Facebook, Twitter, LinkedIn) offer API access to user data. Access must comply with their terms of service and privacy policies.
+
+10. **Machine Learning Models from Fraud Detection Vendors**:
+    - Various vendors offer fraud detection solutions, including SAS, FICO, and Feedzai.
+
+11. **Government and Law Enforcement Data**:
+    - Collaboration with local law enforcement agencies and government entities is typically done on a case-by-case basis, with data shared under legal agreements.
+
+12. **Industry-Specific Data**:
+    - The availability of industry-specific data sources varies widely. Organizations may need to partner with industry consortiums or data providers specific to their sector.
+
+13. **Open Source Threat Intelligence**:
+    - Open source threat intelligence feeds can be obtained from various sources such as the Open Threat Exchange (OTX) by AlienVault (now part of AT&T Cybersecurity) and various cybersecurity communities.
