f5a92276f939627d04f0a10cd3f438bcb33d26fc
codeguru <139157705+codeguru185@users.noreply.github.com>
2023-09-20 09:24:51 -0500

    Update new.py

--- new.py
+++ new.py
@@ -1,55 +1,68 @@
+import pyodbc
+import psycopg2
 
-1. **Credit Bureaus**:
-   - Equifax
-   - Experian
-   - TransUnion
+# SQL Server database connection parameters
+sql_server_conn_str = (
+    "Driver={SQL Server Native Client 11.0};"
+    "Server=SQL_SERVER_HOST;"
+    "Database=SQL_SERVER_DB_NAME;"
+    "UID=SQL_SERVER_USERNAME;"
+    "PWD=SQL_SERVER_PASSWORD;"
+)
 
-2. **Transaction Data Providers**:
-   - Fiserv
-   - ACI Worldwide
-   - First Data (now part of Fiserv)
+# Greenplum database connection parameters
+greenplum_conn_str = (
+    "dbname=GREENPLUM_DB_NAME "
+    "user=GREENPLUM_USERNAME "
+    "password=GREENPLUM_PASSWORD "
+    "host=GREENPLUM_HOST "
+    "port=GREENPLUM_PORT"
+)
 
-3. **Identity Verification Services**:
-   - Jumio
-   - Trulioo
-   - Onfido
+# SQL query to select data from SQL Server table
+sql_query = "SELECT * FROM SQL_SERVER_TABLE_NAME"
 
-4. **Geolocation Data**:
-   - MaxMind
-   - Neustar
-   - IPinfo
+try:
+    # Connect to SQL Server
+    sql_server_conn = pyodbc.connect(sql_server_conn_str)
+    sql_cursor = sql_server_conn.cursor()
 
-5. **Device Fingerprinting**:
-   - ThreatMetrix (a LexisNexis Risk Solutions company)
-   - iovation (a TransUnion company)
-   - BioCatch
+    # Execute the SQL query
+    sql_cursor.execute(sql_query)
 
-6. **Behavioral Biometrics**:
-   - BioCatch
-   - BehavioSec
-   - NuData Security (a Mastercard company)
+    # Fetch data from SQL Server
+    sql_data = sql_cursor.fetchall()
 
-7. **IP Reputation Services**:
-   - Spamhaus
-   - Barracuda Central
-   - Cisco Talos
+    # Connect to Greenplum
+    greenplum_conn = psycopg2.connect(greenplum_conn_str)
+    greenplum_cursor = greenplum_conn.cursor()
 
-8. **Payment Processor Data**:
-   - PayPal
-   - Stripe
-   - Square
+    # Greenplum table name to load data into
+    greenplum_table_name = "GREENPLUM_TABLE_NAME"
 
-9. **Social Media Data**:
-   - Various social media platforms (e.g., Facebook, Twitter, LinkedIn) offer API access to user data. Access must comply with their terms of service and privacy policies.
+    # Prepare the INSERT statement for Greenplum
+    insert_query = f"INSERT INTO {greenplum_table_name} VALUES %s"
 
-10. **Machine Learning Models from Fraud Detection Vendors**:
-    - Various vendors offer fraud detection solutions, including SAS, FICO, and Feedzai.
+    # Load data into Greenplum
+    psycopg2.extras.execute_values(
+        greenplum_cursor,
+        insert_query,
+        sql_data,  # Data fetched from SQL Server
+        template=None,
+        page_size=1000,  # Adjust as needed
+    )
 
-11. **Government and Law Enforcement Data**:
-    - Collaboration with local law enforcement agencies and government entities is typically done on a case-by-case basis, with data shared under legal agreements.
+    # Commit the transaction
+    greenplum_conn.commit()
 
-12. **Industry-Specific Data**:
-    - The availability of industry-specific data sources varies widely. Organizations may need to partner with industry consortiums or data providers specific to their sector.
+    print("Data loaded successfully into Greenplum.")
 
-13. **Open Source Threat Intelligence**:
-    - Open source threat intelligence feeds can be obtained from various sources such as the Open Threat Exchange (OTX) by AlienVault (now part of AT&T Cybersecurity) and various cybersecurity communities.
+except Exception as e:
+    print(f"Error: {str(e)}")
+
+finally:
+    # Close database connections
+    if sql_server_conn:
+        sql_server_conn.close()
+    if greenplum_conn:
+        greenplum_conn.close()
